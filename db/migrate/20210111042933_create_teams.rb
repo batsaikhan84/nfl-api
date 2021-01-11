8073bd91b5a176a77c3d0b2cc04056d2de055022
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2021-01-10 22:29:56 -0600

    create team controller

---
+++ db/migrate/20210111042933_create_teams.rb
@@ -0,0 +1,12 @@
+class CreateTeams < ActiveRecord::Migration[6.0]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.string :city
+      t.string :conference
+      t.string :gameID
+
+      t.timestamps
+    end
+  end
+end
