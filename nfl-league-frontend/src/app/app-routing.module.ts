b0c2ddfc98dce6ec8f1faa6d48bb60da12758149
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2021-01-11 21:35:12 -0600

    implement router link

--- nfl-league-frontend/src/app/app-routing.module.ts
+++ nfl-league-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { TeamsComponent } from './components/teams/teams.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'teams', component: TeamsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
