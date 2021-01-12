f193bce8996ca9c113a20208ea0d862b561de5f8
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2021-01-11 21:24:40 -0600

    create navbar

--- nfl-league-frontend/src/app/app.module.ts
+++ nfl-league-frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TeamsComponent } from './components/teams/teams.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TeamsComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
