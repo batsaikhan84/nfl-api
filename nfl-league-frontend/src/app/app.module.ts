b9057b46d38c97d5ab15ebc4442c445c1d4ba12e
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2021-01-11 20:26:09 -0600

    create header component

--- nfl-league-frontend/src/app/app.module.ts
+++ nfl-league-frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
