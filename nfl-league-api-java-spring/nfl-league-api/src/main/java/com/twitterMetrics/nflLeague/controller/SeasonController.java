d609073538f13eef3a9b978a47c3df91ed10b5a1
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2021-01-16 10:49:27 -0600

    implement get mapping

--- nfl-league-api-java-spring/nfl-league-api/src/main/java/com/twitterMetrics/nflLeague/controller/SeasonController.java
+++ nfl-league-api-java-spring/nfl-league-api/src/main/java/com/twitterMetrics/nflLeague/controller/SeasonController.java
@@ -1,12 +1,17 @@
 package com.twitterMetrics.nflLeague.controller;
 
-import com.twitterMetrics.nflLeague.repository.TeamRepository;
+import com.twitterMetrics.nflLeague.model.Season;
+import com.twitterMetrics.nflLeague.repository.SeasonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SeasonController {
     @Autowired
-    private TeamRepository teamRepository;
-
+    private SeasonRepository seasonRepository;
+    @GetMapping("/seasons")
+    public List<Season> getSeasons() { return seasonRepository.findAll(); }
 }
